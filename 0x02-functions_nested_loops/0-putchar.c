804f194bd344a5b27c54c5ae9bfee435a90f98f7
M3siCS <hadeelkaddy86@gmail.com>
2023-06-20 16:45:19 -0700

    H&M

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Discription: print _putchar using putchar prototype
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchr(star[]);
+	_puthcar('\n');
+
+	return (0);
+}
+
