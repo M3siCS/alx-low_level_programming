4ff1845c041b00311b74c9337f8e02cc82e1cc06
M3siCS <hadeelkaddy86@gmail.com>
2023-06-19 17:47:33 -0700

    H&M

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main -entry point
  *
  * Discription: print vaule of n statuse:
- * 		greater than,is zero and is not less than 6.
+ *		greater than,is zero and is not less than 6.
  *
  * Return: 0 (success)
 */
