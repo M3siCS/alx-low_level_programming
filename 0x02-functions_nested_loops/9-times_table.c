9fa17f0f3507f9cc9356401dce9236e468e48a1d
M3siCS <hadeelkaddy86@gmail.com>
2023-06-21 14:44:28 -0700

    H&M

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,7 +33,7 @@ void times_table(void)
 				_putchar((prod / 10) + 48); /*get the first digit*/
 			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
-		_putchat('\n');
+		_putchar('\n');
 	}
 
 }
