b53f05e4d973d7dee405b222d592408fc8660852
M3siCS <hadeelkaddy86@gmail.com>
2023-06-21 16:29:24 -0700

    H&M

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,23 +16,23 @@ void print_times_taple(int n)
 			_putchar(48);
 			for (mult = 1; mult <= n; mult++)
 			{
-				_putrach(',');
-				_putrach(' ');
+				_putchar(',');
+				_putchar(' ');
 
 				prod = num * mult;
 
 				if (prod <= 9)
-					_putrach(' ');
+					_putchar(' ');
 				if (prod <= 99)
-					_putrach(' ');
+					_putchar(' ');
 
 				if (prod <= 100)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) % 10 + 48);
-				} else if (prod <= 99 && prod >= 10);
+				} else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48);	
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
