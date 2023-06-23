884842e6d8b04342c77b9c1c4914649f18164cd1
M3siCS <hadeelkaddy86@gmail.com>
2023-06-23 04:11:41 -0700

    H&M

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -25,7 +25,7 @@ void print_diagonal(int n)
 			for (space = 1; space <= postn; space++)
 				_putchar(' ');
 			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
