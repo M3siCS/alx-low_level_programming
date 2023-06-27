47d09555ef5bc1941a6cf57b9e9bba544c103dba
M3siCS <hadeelkaddy86@gmail.com>
2023-06-26 18:43:35 -0700

    H&M

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,15 +8,16 @@
 int main(void)
 {
 	int sum;
-	time_t c;
+	char c;
 
 	srand(time(NULL));
-	while (sum <= 26450)
+	while (sum <= 2645)
 	{
 		c = rand() % 128;
-		sum += c ;
+		sum += c;
 		putchar(c);
 	}
 	putchar(2772 - sum);
+
 	return (0);
 }
