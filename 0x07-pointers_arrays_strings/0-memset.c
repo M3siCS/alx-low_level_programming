b6140f48a5723779fe4011902f19ff76000aecec
M3siCS <hadeelkaddy86@gmail.com>
2023-07-03 16:02:38 -0700

    a

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memeset - constant byte of fills memory
+ * @s : is a pointer to put the constant
+ * @b : is the constant
+ * @n : the max bytes to use
+ *
+ * return : pointer s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int itsr;
+
+	for (itsr = 0; n > 0; itsr++, n--)
+	{
+		s[itsr] = b;
+	}
+
+	return (s);
+}
