700a8d33faa2b5811a3c0d5b6103abdddf5f4415
M3siCS <hadeelkaddy86@gmail.com>
2023-07-14 14:34:43 -0700

    m

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,11 +10,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *m = malloc(b);
+	void *ptr = malloc(b);
 
-	if (m == NULL)
+	if (ptr == NULL)
 		exit(98);
 
-	return (m);
+	return (ptr);
 }
 
