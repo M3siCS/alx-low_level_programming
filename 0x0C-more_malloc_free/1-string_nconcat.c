a71c88c31a41ff187a2ab57dbbe6da4dfea37595
M3siCS <hadeelkaddy86@gmail.com>
2023-07-14 14:50:28 -0700

    m

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * string_nconcat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: amount of bytes.
- *
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0;
-	char *concatenated;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
-	if (n >= len2)
-		n = len2;
-
-	concatenated = malloc(sizeof(char) * (len1 + n + 1));
-
-	if (concatenated == NULL)
+	while (s1[size1] != '\0')
 	{
-		return (NULL);
+		size1++;
 	}
 
-	strncpy(concatenated, s1, len1);
-	strncat(concatenated, s2, n);
-	concatenated[len1 + n] = ('\0');
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
 
-	return (concatenated);
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
-
