17135e81069b2dbe896eedf614bd60c4c5ed06de
M3siCS <hadeelkaddy86@gmail.com>
2023-09-04 13:07:32 -0700

    done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,8 @@
  * @filename: A pointer to the name of the file.
  * @text_content: The string to add to the end of the file.
  *
- * Return: If the function fails, filename is NULL, or the user lacks write permissions - return -1.
+ * Return: If the function fails, filename is NULL,
+ *or the user lacks write permissions - return -1.
  * If the file does not exist - return -1.
  * Otherwise, return 1 on success.
 */
